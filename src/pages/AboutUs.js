5b770da50d9388e4d39823997885ff23aa589cbe
anjalbaral <anjalbaral7@gmail.com>
2022-05-06 20:02:04 +0545

    updates

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,27 @@
 import React from "react";
+import aboutuscover from "../assets/images/aboutuscover.jpg";
+import { Parallax } from "react-parallax";
 
 function AboutUs() {
 	return (
-		<div style={{ width: "100%", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-			<span>About us page</span>
+		<div className="about-us">
+			<Parallax
+				className="cover-parent"
+				style={{ minWidth: "100%" }}
+				blur={{ min: -10, max: 10 }}
+				strength={200}
+				bgClassName="parallexComp"
+				bgImageStyle={{ width: "100%", backgroundSize: "100%" }}
+				bgImage={aboutuscover}
+			>
+				<div className="about-us__cover">
+					<div className="about-us__cover__overlay"></div>
+					<div className="about-us__cover__content">
+						<h3>About Us</h3>
+						<span>Welcome to vedalaya group</span>
+					</div>
+				</div>
+			</Parallax>
 		</div>
 	);
 }
