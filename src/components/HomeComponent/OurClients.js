e0898e4f54a2ee15edb2786f61fba44c1d512543
anjalbaral <anjalbaral7@gmail.com>
2022-05-16 09:59:44 +0545

    updates

--- src/components/HomeComponent/OurClients.js
+++ src/components/HomeComponent/OurClients.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { GiHouse, GiWhiteBook } from "react-icons/gi";
+import { FaMoneyCheckAlt, FaHotel, FaIndustry } from "react-icons/fa";
+import { MdRealEstateAgent } from "react-icons/md";
+import { RiBuildingFill, RiGovernmentFill } from "react-icons/ri";
 
 function OurClients() {
 	return (
 		<div className="home__our-clients">
-			<div className="home__our-clients__header">OUR CLIENTS</div>
+			<div className="home__our-clients__header">OUR WORKING AREAS</div>
 			<div className="underline-separator">
 				<div className="line"></div>
 				<div className="mid"></div>
@@ -11,34 +15,52 @@ function OurClients() {
 			</div>
 			<div className="home__our-clients__clients">
 				<div className="home__our-clients__clients__client">
-					<img src="https://senyumpeople.com/wp-content/uploads/2015/10/shopee.png"></img>
+					<div className="home__our-clients__clients__client__top">
+						<GiHouse />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Residental</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://img.freepik.com/free-vector/poultry-logo-animal-farm-company-icon-with-hen-symbol_18099-921.jpg" />
+					<div className="home__our-clients__clients__client__top">
+						<FaMoneyCheckAlt />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Commercial</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://img.freepik.com/free-vector/animal-farm-logo-poultry-logo-design-template_18099-586.jpg"></img>
+					<div className="home__our-clients__clients__client__top">
+						<FaHotel />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Hotel & Tourism</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://static.vecteezy.com/system/resources/thumbnails/002/373/577/small/rooster-icon-logo-design-template-illustration-vector.jpg"></img>
+					<div className="home__our-clients__clients__client__top">
+						<GiWhiteBook />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Education</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://thumbs.dreamstime.com/b/vintage-chicken-farm-logo-design-illustration-vector-vintage-chicken-farm-logo-design-illustration-vector-170854850.jpg"></img>
+					<div className="home__our-clients__clients__client__top">
+						<FaIndustry />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Industrial</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-simple-real-estate-logo-template-vector-logo-for-business-and-company-png-image_5369966.jpg"></img>
+					<div className="home__our-clients__clients__client__top">
+						<MdRealEstateAgent />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Real estate</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://st4.depositphotos.com/29475518/39034/v/380/depositphotos_390348544-stock-illustration-letter-initial-logo-design-vector.jpg?forcejpeg=true"></img>
+					<div className="home__our-clients__clients__client__top">
+						<RiBuildingFill />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Rental Retails</div>
 				</div>
 				<div className="home__our-clients__clients__client">
-					<img src="https://png.pngtree.com/template/20200907/ourlarge/pngtree-h-home-logo-modern-clean-simple-designs-with-black-color-and-image_410891.jpg"></img>
-				</div>
-				<div className="home__our-clients__clients__client">
-					<img src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-healthy-food-logo-designs-png-image_4157609.jpg"></img>
-				</div>
-				<div className="home__our-clients__clients__client">
-					<img src="https://static.vecteezy.com/system/resources/previews/000/603/783/non_2x/vector-chef-logo-food-and-drink-logo-restaurant-logo-design-concept-t.jpg"></img>
+					<div className="home__our-clients__clients__client__top">
+						<RiGovernmentFill />
+					</div>
+					<div className="home__our-clients__clients__client__bottom">Governmental</div>
 				</div>
 			</div>
 		</div>
